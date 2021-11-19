bbeec508002684834af8e91a29cd73d4bdf54c44
Eduardo Cristiano Thums <eduardocristiano01@gmail.com>
2021-11-19 12:50:45 -0300

    Add menu on script

--- script.py
+++ script.py
@@ -2,28 +2,30 @@
 from cryptography.hazmat.primitives._serialization import Encoding, PrivateFormat, NoEncryption, PublicFormat
 
 from cryptography.hazmat.primitives.asymmetric import rsa
+from time import sleep
+from os import getcwd
 
 KEY_SIZE_IN_BITS = 512
 
 
-class App:
+class Encrypter:
 
-    def encrypt(self):
-        # name = input('Insert your name for the key pair be generated: ')
-        name = 'eduardo'
+    # def generate_(self):
+    # name = input('Insert your name for the key pair be generated: ')
+    # name = 'eduardo'
 
-        self._generate_key_pair(name)
+    # self._generate_key_pair(name)
 
-        # file_path = input('Insert the full path of the file to be encrypted(Ex: /path/to/file.txt): ')
-        file_path = '/home/eduardothums/Downloads/file.txt'
-        self._encrypt_file(file_path)
-
-    def decrypt(self):
-        pass
+    # # file_path = input('Insert the full path of the file to be encrypted(Ex: /path/to/file.txt): ')
+    # file_path = '/home/eduardothums/Downloads/file.txt'
+    # self._encrypt_file(file_path)
 
     @staticmethod
-    def _generate_key_pair(name: str):
-        print(f'Generating key pair for {name}')
+    def generate_key_pair():
+        # name = input('Insert your name for the key pair be generated: ')
+        name = 'eduardo'
+        print(f'Generating key pair for {name}...')
+        sleep(1)
 
         private_key = rsa.generate_private_key(
             public_exponent=65537,
@@ -36,7 +38,7 @@ class App:
             encryption_algorithm=NoEncryption()
         )
 
-        with open(f'{name}.pr', 'wb') as file:
+        with open(f'./src/{name}.pr', 'wb') as file:
             file.write(private_bytes)
 
         public_key = private_key.public_key()
@@ -46,30 +48,108 @@ class App:
             format=PublicFormat.SubjectPublicKeyInfo
         )
 
-        with open(f'{name}.pu', 'wb') as file:
+        with open(f'./src/{name}.pu', 'wb') as file:
             file.write(public_bytes)
+
+        print(f'Private key generated at {getcwd()}/{name}.pr')
+        print(f'Public key generated at {getcwd()}/{name}.pr')
 
     def _encrypt_file(self, file_path):
         pass
 
 
-if __name__ == '__main__':
-    print('How you wanna use the CLI?')
-    print('E - Encrypt something')
-    print('D - Decrypt something')
+class Menu:
 
-    step = input()
+    def display_main(self):
+        print('Welcome to the AES CBC CLI!')
 
-    if step not in ['E', 'D']:
-        print('Invalid action! Exiting...')
+        choice = None
+
+        while choice != 3:
+            print('\n:::::::::::::::::')
+            print('::: MAIN MENU :::')
+            print(':::::::::::::::::\n')
+            print('1 - Encryption')
+            print('2 - Decryption')
+            print('3 - Exit')
+
+            try:
+                choice = int(input('>>> '))
+
+            except ValueError:
+                self.exit_with_error('Invalid entry')
+
+            if choice not in [1, 2, 3]:
+                self.exit_with_error('Unknown menu entry')
+
+            if choice == 1:
+                self.display_encrypt_menu()
+
+            elif choice == 2:
+                print('Decrpyt')
+
+            else:
+                self.exit_with_success()
+
+    def display_encrypt_menu(self):
+        choice = None
+
+        while choice != 4:
+            print('\n::::::::::::::::::::')
+            print('::: ENCRYPT MENU :::')
+            print('::::::::::::::::::::\n')
+            print('1 - Generate key pair')
+            print('2 - Encrypt plain text file (symmetric mode)')
+            print('3 - Encrypt file (asymmetric mode)')
+            print('4 - Back to main menu')
+            print('5 - Exit')
+
+            try:
+                choice = int(input('>>> '))
+
+            except ValueError:
+                self.show_invalid_entry()
+                continue
+
+            if choice not in [1, 2, 3, 4, 5]:
+                self.show_unknown_entry()
+                continue
+
+            encrypter = Encrypter()
+
+            if choice == 1:
+                encrypter.generate_key_pair()
+
+            elif choice == 2:
+                print('encrpy plain text file')
+
+            elif choice == 3:
+                print('encrpy file')
+
+            elif choice == 5:
+                self.exit_with_success()
+
+    @staticmethod
+    def exit_with_error(message: str):
+        print(f'{message}! Exiting...')
         exit(1)
 
-    app = App()
+    @staticmethod
+    def exit_with_success():
+        print('Exiting...')
+        sleep(1)
+        exit(0)
 
-    if step == 'E':
-        print('===ENCRYPT===')
-        app.encrypt()
+    @staticmethod
+    def show_invalid_entry():
+        print('Invalid entry! Returning...')
 
-    else:
-        print('===DECRYPT===')
-        app.decrypt()
+    @staticmethod
+    def show_unknown_entry():
+        print('Unknown entry! Returning...')
+
+
+if __name__ == '__main__':
+    menu = Menu()
+
+    menu.display_main()
